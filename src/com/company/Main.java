3a2bab79abc683f9fa1d0b317112645b54eda351
jkronm <jacob.kronman@gmail.com>
2021-12-31 09:20:01 -0600

    Updated Main to include toggle for other games.  Place holders for now.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,13 +23,41 @@ public class Main {
 
         player.setAmount(repInt);
 
-        // play blackjack
+        // pick a game
         do {
-        BlackJackTable newGame = new BlackJackTable();
-        newGame.playGame(player);
-        System.out.println("Would you like to play a whole new game? (N/Y)");
+            System.out.println("Pick a game (i.e. 1, 2, or 3):");
+            System.out.println("1) BlackJack");
+            System.out.println("2) Place Holder");
+            System.out.println("3) Place Holder");
             reply = sc.nextLine();
-        }
-        while (reply.equalsIgnoreCase("Y"));
+            try {
+                repInt = Integer.parseInt(reply);
+            }
+            catch (NumberFormatException e)
+            {
+                System.out.println("Must be an integer.");
+            } //////////////// handler just outputs error and fixes nothing for now.
+
+            switch (repInt) {
+                case 1 : {
+                    BlackJackTable newGame = new BlackJackTable();
+                    newGame.playGame(player);
+                    break;
+                }
+                case 2 : System.out.println("No game avialable yet."); break;
+                case 3 : System.out.println("No game avialable yet."); break;
+            }
+            System.out.println("Would you like to play a different new game? (N/Y)");
+            reply = sc.nextLine();
+        } while (reply.equalsIgnoreCase("Y"));
+
+        //// play blackjack
+        //do {
+        //BlackJackTable newGame = new BlackJackTable();
+        //newGame.playGame(player);
+        //System.out.println("Would you like to play a whole new game? (N/Y)");
+        //    reply = sc.nextLine();
+        //}
+        //while (reply.equalsIgnoreCase("Y"));
     }
 }
