7ca47ee52fb0291fae69a38e5e83ad9e327bb692
jkronm <jacob.kronman@gmail.com>
2022-01-02 15:17:00 -0600

    1) Updated BlackJackTable with better checks on player funds. 2) Built infrustructure for "Split" action in BlackJackTable. 3) Updated Hand class and and HandTest.
    FIXES NEEDED:
    
    1) When trying second split, below is the result:
    Player's Hand:
    Hand 0:  * TWO|CLUB * FOUR|DIAMOND
    Hand 1:  * TWO|CLUB * SIX|HEART * FOUR|CLUB
    Hand 2:  * TWO|DIAMOND
    2) BlackJack lets you replay hand 1 if you bust on hand 2.  Throws exception, but still tries.

--- src/com/company/BlackJackTable.java
+++ src/com/company/BlackJackTable.java
@@ -1,19 +1,23 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class BlackJackTable {
 
     Shoe shoe;
     Hand dealerHand;
-    Hand playerHand; // make this a list of hands. then add run checks below against all instances of hands.
-    int betOnTable = 0;
+    List<Hand> playerHands;
+    List<Integer> betOnTable;
     int minimumBet = 1;
 
     public BlackJackTable() {
         shoe = new Shoe();
         dealerHand = new Hand();
-        playerHand = new Hand();
+        playerHands = new ArrayList<Hand>();
+        betOnTable = new ArrayList<Integer>(); betOnTable.add(0);
+        //playerHand = new Hand();
     }
 
     // Initilize blackjack game. Starts with check on player funds
@@ -48,7 +52,7 @@ public class BlackJackTable {
         String reply = "";
         int repInt = 0;
 
-        // place bests
+        // place initial bests
         do {
             System.out.println("Place your Bet. (enter integer amount between $" + minimumBet + " and " + player.getAmountString() + ").");
             reply = sc.nextLine();
@@ -60,54 +64,88 @@ public class BlackJackTable {
             if (checkEnoughMoney(repInt, player) != false) {break;}
         } while (true);
         player.withdraw(repInt);
-        betOnTable += repInt;
+        betOnTable.set(betOnTable.get(0), betOnTable.get(0) + repInt);
 
-        // deal
-        playerHand.empty();
+        // initial deal
+        {
+        playerHands.removeAll(playerHands);
+
+        Hand firstHand = new Hand();
+        playerHands.add(firstHand);
+
+        for (Hand playerHand : playerHands) {playerHand.empty();}
         dealerHand.empty();
 
-        playerHand.add(shoe.draw());
+        for (Hand playerHand : playerHands) {
+            //playerHand.add(shoe.draw()); // <><><><> replace when done testing
+            playerHand.add(new Card(Value.TWO, Suit.CLUB)); //<><><>< for testing
+        }
         dealerHand.add(shoe.draw());
-        playerHand.add(shoe.draw());
+        for (Hand playerHand : playerHands) {
+            //playerHand.add(shoe.draw()); // <><><><> replace when done testing
+            playerHand.add(new Card(Value.TWO, Suit.CLUB)); //<><><>< for testing
+        }
         dealerHand.add(shoe.draw());
+        }
 
-        do {
-            showTable(false);
-            System.out.println("Would player like to Stand, Hit, Double down, or SPlit? (S/H/D/SP)");
-            reply = sc.nextLine().toUpperCase();
-            switch (reply) {
-                case "S" : {break;}
-                case "H" : {playerHand.add(shoe.draw()); break;}
-                case "D" : {
-                    if (checkEnoughMoney(repInt, player) == false) {
-                        System.out.println("You receive a card, but no additional bet (a HIT).");
-                        playerHand.add(shoe.draw());
-                    }
-                    else {
-                        playerHand.add(shoe.draw());
-                        player.withdraw(repInt);
-                        betOnTable += repInt;
-                    }
-                    break;
-                }
-                case "SP" : {
-                    if (playerHand.cards.get(0).getValue() != playerHand.cards.get(0).getValue()) {
-                        System.out.println("Cannot split, cards are not the same value. No additional cards or bet (a Stand).");
+        // player decide what to Stand, Hit, Double down, or SPlit
+        for (int handIndex = 0; handIndex < playerHands.size(); handIndex++) {
+            do {
+                showTable(false);
+                if (playerHands.size() > 1) {System.out.print("Hand " + (handIndex + 1) + ": ");}
+                System.out.println("Would player like to Stand, Hit, Double down, or SPlit? (S/H/D/SP)");
+                reply = sc.nextLine().toUpperCase();
+                switch (reply) {
+                    case "S": {
                         break;
                     }
-                    // add this at some point
-                    break;
+                    case "H": {
+                        playerHands.get(handIndex).add(shoe.draw());
+                        break;
+                    }
+                    case "D": {
+                        if (checkEnoughMoney(repInt, player) == false) {
+                            System.out.println("You receive a card, but no additional bet (a HIT).");
+                            playerHands.get(handIndex).add(shoe.draw());
+                        } else {
+                            playerHands.get(handIndex).add(shoe.draw());
+                            player.withdraw(repInt);
+                            betOnTable.set(handIndex, betOnTable.get(handIndex) * 2);
+                        }
+                        break;
+                    }
+                    case "SP": {
+                        if (playerHands.get(handIndex).cards.get(0).getValue() != playerHands.get(handIndex).cards.get(1).getValue()) {
+                            System.out.println("Cannot split, cards are not the same value. No additional cards or bet (a Stand).");
+                            break;
+                        }
+                        else if (checkEnoughMoney(betOnTable.get(handIndex), player) == false) {
+                            System.out.println("You receive a card for your original hand with no additional bet (a HIT).");
+                            playerHands.get(handIndex).add(shoe.draw());
+                        }
+                        else {
+                            Hand newHand = new Hand();
+                            newHand.add(playerHands.get(handIndex).removeAndReturnTopCard()); // get most recent card
+                            playerHands.add(newHand);
+                            playerHands.get(handIndex).add(shoe.draw());
+                            playerHands.get(handIndex + 1).add(shoe.draw());
+                            betOnTable.add(betOnTable.get(handIndex));  // places identical bet from original hand on new hand.
+                            player.withdraw(betOnTable.get(handIndex));
+                            break;
+                        }
+                    }
+                }
+
+                if (playerHands.get(handIndex).value() > 21) {
+                    showTable(false);
+                    System.out.println("You busted this hand!");
+                    playerHands.remove(handIndex);
+                    betOnTable.remove(handIndex);
+                    //betOnTable.set(handIndex, 0); ///<><><> going out of bounds somehow.
                 }
             }
-
-            if (playerHand.value() > 21) {
-                showTable(false);
-                System.out.println("You Busted!");
-                betOnTable = 0;
-                return;
-            }
+            while (reply.equalsIgnoreCase("H") || reply.equalsIgnoreCase("SP"));
         }
-        while (reply.equalsIgnoreCase("H"));
 
         // process dealer hand
         dealerDecision();
@@ -115,31 +153,36 @@ public class BlackJackTable {
         // show final table (with all dealer cards)
         showTable(true);
 
-        // comparisons made win, lose, tie
-        if (playerHand.value() <=21 && (dealerHand.value() > 21  || playerHand.value() > dealerHand.value())) {
-            if (dealerHand.value() > 21) {
-                System.out.print("Dealer Busts, ");
+        // comparisons made win, lose, tie (already checked if player busted)
+        for (int handIndex = 0; handIndex < playerHands.size(); handIndex++) {
+            if (playerHands.size() > 1) {System.out.print("Hand " + (handIndex + 1) + ": ");}
+            if (playerHands.get(handIndex).value() == dealerHand.value()) {
+                System.out.println("You Pushed the House!");
+                player.deposit(betOnTable.get(handIndex));
+                betOnTable.set(handIndex, 0);
             }
-            System.out.println("You Win!");
-            player.deposit(betOnTable * 2);
-            betOnTable = 0;
-            return;
+            else if (playerHands.get(handIndex).value() <= 21 && (dealerHand.value() > 21 || playerHands.get(handIndex).value() > dealerHand.value())) {
+                if (dealerHand.value() > 21) {
+                    System.out.print("Dealer Busts, ");
+                }
+                System.out.println("You Win!");
+                player.deposit(betOnTable.get(handIndex) * 2);
+                betOnTable.set(handIndex, 0);
+                //return; //
+            }
+            else {
+                System.out.println("You Lose!");
+                betOnTable.set(handIndex, 0);
+            }
         }
-        if (playerHand.value() == dealerHand.value()) {
-            System.out.println("You Pushed the House!");
-            player.deposit(betOnTable);
-            betOnTable = 0;
-        }
-        else {
-            System.out.println("You Lose!");
-            betOnTable = 0;
-        }
+        return;
     }
 
     // output table results
     private void showTable(boolean finalHand) {
         System.out.println("###################");
-        System.out.println("Dealer's Hand:");
+        System.out.println("Dealer:");
+        System.out.print("Hand: ");
         if (!finalHand) {
             System.out.print(" * " + dealerHand.cardsInHand().get(0));
         }
@@ -151,9 +194,15 @@ public class BlackJackTable {
         System.out.println();
         System.out.println("-------------------");
 
-        System.out.println("Player's Hand:");
-        for (Card card : playerHand.cardsInHand()) {
-            System.out.print(" * " + card);
+        System.out.print("Player's Hand:");
+        for (int handIndex = 0; handIndex < playerHands.size(); handIndex++){
+            System.out.println();
+            System.out.print("Hand");
+            if (playerHands.size() > 1) {System.out.print(" " + (handIndex + 1)); }
+            System.out.print(": ");
+            for (Card card : playerHands.get(handIndex).cardsInHand()) {
+                System.out.print(" * " + card);
+            }
         }
         System.out.println();
         System.out.println("###################");
